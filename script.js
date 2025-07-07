f6d8a5a6dbf80665b42282047ae2bb3898d99862
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-07-07 15:18:56 +0200

    added  rendering likes

--- script.js
+++ script.js
@@ -1,193 +1,198 @@
 
 let books = [
-    {
-      "name": "Die Geheimnisse des Ozeans",
-      "author": "Clara Meer",
-      "likes": 1250,
-      "liked": true,
-      "price": 19.99,
-      "publishedYear": 2018,
-      "genre": "Fantasy",
-      "comments": [
-        {
-          "name": "Leser123",
-          "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
-        },
-        {
-          "name": "Bookworm84",
-          "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
-        },
-        {
-          "name": "FantasyFanatic",
-          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
-        },
-        {
-          "name": "SciFiGuru",
-          "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
-        },
-        {
-          "name": "NovelLover",
-          "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
-        }
-      ]
-    },
-    {
-      "name": "Der vergessene Pfad",
-      "author": "Maximilian Schwarz",
-      "likes": 980,
-      "liked": false,
-      "price": 14.50,
-      "publishedYear": 2021,
-      "genre": "Fantasy",
-      "comments": []
-    },
-    {
-      "name": "Die Farben des Himmels",
-      "author": "Laura Blau",
-      "likes": 1520,
-      "liked": true,
-      "price": 22.95,
-      "publishedYear": 2019,
-      "genre": "Romantik",
-      "comments": [
-        {
-          "name": "LeserPeter",
-          "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
-        },
-        {
-          "name": "BookLover21",
-          "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
-        },
-        {
-          "name": "FantasyNerd",
-          "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
-        },
-        {
-          "name": "SciFiEnthusiast",
-          "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
-        },
-        {
-          "name": "ReadingAddict",
-          "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das Rätsel der Zeit",
-      "author": "Alexander Weiss",
-      "likes": 750,
-      "liked": false,
-      "price": 18.00,
-      "publishedYear": 2020,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "BuchKenner",
-          "comment": "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat."
-        },
-        {
-          "name": "LeseWurm",
-          "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
-        }
-      ]
-    },
-    {
-      "name": "Der letzte Wächter",
-      "author": "Sabine Grün",
-      "likes": 1300,
-      "liked": true,
-      "price": 16.75,
-      "publishedYear": 2017,
-      "genre": "Fantasy",
-      "comments": []
-    },
-    {
-      "name": "Im Schatten des Mondes",
-      "author": "Philipp Silber",
-      "likes": 890,
-      "liked": false,
-      "price": 12.30,
-      "publishedYear": 2022,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "BücherLiebhaber",
-          "comment": "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd."
-        },
-        {
-          "name": "Leseratte",
-          "comment": "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat."
-        }
-      ]
-    },
-    {
-      "name": "Jenseits der Sterne",
-      "author": "Oliver Schwarz",
-      "likes": 1450,
-      "liked": true,
-      "price": 21.00,
-      "publishedYear": 2015,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "name": "Leser123",
-          "comment": "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das verborgene Königreich",
-      "author": "Elena Gold",
-      "likes": 920,
-      "liked": false,
-      "price": 17.50,
-      "publishedYear": 2020,
-      "genre": "Fantasy",
-      "comments": [
-        {
-          "name": "Bookworm92",
-          "comment": "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat."
-        }
-      ]
-    },
-    {
-      "name": "Liebe in Zeiten des Krieges",
-      "author": "Emilia Rot",
-      "likes": 1800,
-      "liked": true,
-      "price": 19.99,
-      "publishedYear": 2016,
-      "genre": "Romantik",
-      "comments": [
-        {
-          "name": "Bibliophile23",
-          "comment": "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen."
-        },
-        {
-          "name": "StorySeeker",
-          "comment": "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat."
-        },
-        {
-          "name": "SciFiExplorer",
-          "comment": "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig."
-        }
-      ]
-    }
-  ]
+  {
+    "name": "Die Geheimnisse des Ozeans",
+    "author": "Clara Meer",
+    "likes": 1250,
+    "liked": true,
+    "price": 19.99,
+    "publishedYear": 2018,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "Leser123",
+        "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+      },
+      {
+        "name": "Bookworm84",
+        "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+      },
+      {
+        "name": "FantasyFanatic",
+        "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+      },
+      {
+        "name": "SciFiGuru",
+        "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
+      },
+      {
+        "name": "NovelLover",
+        "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+      }
+    ]
+  },
+  {
+    "name": "Der vergessene Pfad",
+    "author": "Maximilian Schwarz",
+    "likes": 980,
+    "liked": false,
+    "price": 14.50,
+    "publishedYear": 2021,
+    "genre": "Fantasy",
+    "comments": []
+  },
+  {
+    "name": "Die Farben des Himmels",
+    "author": "Laura Blau",
+    "likes": 1520,
+    "liked": true,
+    "price": 22.95,
+    "publishedYear": 2019,
+    "genre": "Romantik",
+    "comments": [
+      {
+        "name": "LeserPeter",
+        "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
+      },
+      {
+        "name": "BookLover21",
+        "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
+      },
+      {
+        "name": "FantasyNerd",
+        "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
+      },
+      {
+        "name": "SciFiEnthusiast",
+        "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
+      },
+      {
+        "name": "ReadingAddict",
+        "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
+      }
+    ]
+  },
+  {
+    "name": "Das Rätsel der Zeit",
+    "author": "Alexander Weiss",
+    "likes": 750,
+    "liked": false,
+    "price": 18.00,
+    "publishedYear": 2020,
+    "genre": "Science-Fiction",
+    "comments": [
+      {
+        "name": "BuchKenner",
+        "comment": "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat."
+      },
+      {
+        "name": "LeseWurm",
+        "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
+      }
+    ]
+  },
+  {
+    "name": "Der letzte Wächter",
+    "author": "Sabine Grün",
+    "likes": 1300,
+    "liked": true,
+    "price": 16.75,
+    "publishedYear": 2017,
+    "genre": "Fantasy",
+    "comments": []
+  },
+  {
+    "name": "Im Schatten des Mondes",
+    "author": "Philipp Silber",
+    "likes": 890,
+    "liked": false,
+    "price": 12.30,
+    "publishedYear": 2022,
+    "genre": "Science-Fiction",
+    "comments": [
+      {
+        "name": "BücherLiebhaber",
+        "comment": "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd."
+      },
+      {
+        "name": "Leseratte",
+        "comment": "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat."
+      }
+    ]
+  },
+  {
+    "name": "Jenseits der Sterne",
+    "author": "Oliver Schwarz",
+    "likes": 1450,
+    "liked": true,
+    "price": 21.00,
+    "publishedYear": 2015,
+    "genre": "Science-Fiction",
+    "comments": [
+      {
+        "name": "Leser123",
+        "comment": "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat."
+      }
+    ]
+  },
+  {
+    "name": "Das verborgene Königreich",
+    "author": "Elena Gold",
+    "likes": 920,
+    "liked": false,
+    "price": 17.50,
+    "publishedYear": 2020,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "Bookworm92",
+        "comment": "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat."
+      }
+    ]
+  },
+  {
+    "name": "Liebe in Zeiten des Krieges",
+    "author": "Emilia Rot",
+    "likes": 1800,
+    "liked": true,
+    "price": 19.99,
+    "publishedYear": 2016,
+    "genre": "Romantik",
+    "comments": [
+      {
+        "name": "Bibliophile23",
+        "comment": "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen."
+      },
+      {
+        "name": "StorySeeker",
+        "comment": "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat."
+      },
+      {
+        "name": "SciFiExplorer",
+        "comment": "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig."
+      }
+    ]
+  }
+]
 
+function init() {
+  render();
+  //likesRendering();
+}
 
-  function render() { // starts with body onload, render 
-                      // the books with the function "pictureRendering"
-    let pictureRef = document.getElementById('content');
+function render() { // init starts with body onload, render 
+  // the books with the function "pictureRendering"
+  let pictureRef = document.getElementById('content');
 
-    for (let index = 0; index < books.length; index++) {
-        pictureRef.innerHTML += booksRendering(index);
-    }
- 
+  for (let index = 0; index < books.length; index++) {
+    pictureRef.innerHTML += booksRendering(index);
+    likesRendering(index);
+  }
+
 }
 
 function booksRendering(index) { // render books  //
-    return ` <div id="book" class="bookfield">
+  return ` <div id="book" class="bookfield">
               <header class="bookHeader"><h3>${books[index].name}</h3></header>
                 <div class="pictureField">
                     <img src="./img/book-pink.png" alt="Illustration from small pichture">
@@ -196,9 +201,8 @@ function booksRendering(index) { // render books  //
                     <div class="priceField">
                         <p>${books[index].price.toFixed(2) + " €"}</p>
                     </div>
-                    <div class="likeField">
-                        <p>${books[index].likes}</p>
-                        <img src="./img/heart_white.svg" alt="Herz">
+                    <div id="like_container_${index}" class="likeField">
+                  
                     </div>
                 </div>
                 <div class="dataField">
@@ -226,11 +230,27 @@ function booksRendering(index) { // render books  //
                     <p>keine Kommentare, schreibe du das erste</p>
                     </div>
                     <div class="inputField">
-                    <input type="text" placeholder="Schreibe dein Kommentar ...">
+                    <input type="text" placeholder="Schreibe dein Kommentar ..." class="input">
                     <img src="./img/send_white.svg" alt="send-icon">
                     </div>
                 </div>
             </div>`
 }
 
-console.log(books[5].name);
+function likesRendering(index) {
+  let likeContainer = document.getElementById(`like_container_${index}`);
+  //console.log(likeContainer);
+  likeContainer.innerHTML += `<p>${books[index].likes}</p>`; //id="likes${index}"
+  if (books[index].liked) {
+    likeContainer.innerHTML += `<img id="unliked${index} class="likeField img" src="./img/heart_pink.svg" alt="red heart for unliked">`
+  }
+  else {
+    likeContainer.innerHTML += `<img id="liked${index} class="likeField img" src="./img/heart_white.svg" alt="white heart for unliked">`
+  }
+}
+
+
+console.log(books[8].name);
+
+//<p>${books[index].likes}</p>
+//                      <img src="./img/heart_white.svg" alt="Herz"></img>
